5480d9ef094b2eced82693fb6276c078bbaf6035
AfridShaik <shaikafrid500@gmail.com>
2025-05-08 18:32:14 +0530

    initial commit for scopped css using JS

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-const secondHand = document.querySelector(".second-hand");
-const minHand = document.querySelector(".min-hand");
-const hourHand = document.querySelector(".hour-hand");
-function setDate() {
-  const now = new Date();
-  const seconds = now.getSeconds();
-  const mintues = now.getMinutes();
-  const hours = now.getHours();
-  const secondsDegrees = (seconds / 60) * 360 + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  const minDegrees = (mintues / 60) * 360 + 90;
-  minHand.style.transform = `rotate(${minDegrees}deg)`;
-  const hourDegrees = (hours / 12) * 360 + (mintues / 60) * 30 + 90;
-  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-  console.log(hours);
+const inputs = document.querySelectorAll(".controls input");
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
 }
-setInterval(setDate, 1000);
+
+inputs.forEach((input) => input.addEventListener("change", handleUpdate));
+inputs.forEach((input) => input.addEventListener("mousemove", handleUpdate));
